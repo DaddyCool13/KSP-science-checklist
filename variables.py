3d1f3f439803ef42234e228e5aa433ed1d15020d
DaddyCool13 <danielfunk@outlook.de>
2024-07-27 11:22:49 +0200

    initial commit

---
+++ variables.py
@@ -0,0 +1,12 @@
+# Hier definiere ich Variablen, für die es im Main File keinen Platz gibt.
+
+celestial_bodies = ["Kerbin", "Minmus", "Mun", "Duna", "Ike", "Dres", "Eve", "Gilly", "Eeloo", "Moho", "Jool", "Pol", "Bop", "Vall", "Tylo", "Laythe", "Sun"]
+celestial_bodies_with_atmosphere_and_water = ["Kerbin", "Eve", "Laythe"]
+celestial_bodies_with_atmosphere = ["Duna", "Jool"]
+
+situations = ["SrfLanded", "SrfSplashed", "FlyingLow", "FlyingHigh", "InSpaceLow", "InSpaceHigh"]
+situations_recovery = ["Landed", "Splashed", "Flew", "SubOrbited", "Orbited", "Flyby"]
+
+experiments = ["surfaceSample", "evaReport", "crewReport", "mysteryGoo", "mobileMaterialsLab", "temperatureScan", "barometerScan", "gravityScan", "seismicScan", "athmosphereAnalysis", "recovery", "evaScience", "infraredTelescope", "magnetometer", "asteroidSample", "cometSample"]
+
+
